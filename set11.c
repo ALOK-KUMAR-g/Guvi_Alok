067f12891b3f7bb3c37e3d4d5ba395a9d2a398b6
Alok <alok.kumar_cs15@gla.ac.in>
2018-01-23 09:06:46 +0530

    set1
    singh

---
+++ set11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int N;
+ {
+   printf("enter the value \n");
+   scanf("%d",&N);
+  if(N<0)
+     printf("Negative");
+   else -if(N>0)
+     printf("Positive");
+	 else
+	   printf("Zero");
+}
+}
