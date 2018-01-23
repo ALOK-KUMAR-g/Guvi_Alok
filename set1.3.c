22194333062e637eee1a192cc063e51bfa9bd980
Alok <alok.kumar_cs15@gla.ac.in>
2018-01-23 22:42:45 +0530

    VOWEL
    COMPLETED

---
+++ set1.3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ char s;
+ {
+  printf("enter your character");
+  scanf("%c",&s);
+  if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||s=='A'||s=='E'||s=='I'||s=='O'||s=='U')
+  printf("vowel");
+  else
+   printf("consonant");
+}
+}
